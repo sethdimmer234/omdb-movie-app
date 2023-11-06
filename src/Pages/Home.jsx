1f8ba7bfc7f5e652b6f458a9bba5fcff6cec8c00
Seth <sethdimmer1@gmail.com>
2023-11-06 13:17:42 -0500

    initial commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+
+function Home() {
+  return (
+    <section>
+      <header>
+        <div className="header__container">
+          <div className="header__description">
+            <h1 className="home-h1">America's Most Awarded Movie Platform</h1>
+            <h2 className="home-h2">Browse for your favorite movies below</h2>
+            <div className="input__container">
+              <input placeholder="Search For Movies Here..." />
+            </div>
+            <button className="btn">
+              <SearchIcon className="searchicon" />
+            </button>
+          </div>
+        </div>
+      </header>
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/Pages/Movieinfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Movieinfo() {
+  return (
+    <div></div>
+  )
+}
+
+export default Movieinfo
